67e1cc9aa5119e8d461a2139197bdd90f44e5a2d
Roshan Chavan <roshan9722@gmail.com>
2017-09-02 11:43:55 +0530

    Update Day_2_Operators.cpp

--- Day_2_Operators.cpp
+++ Day_2_Operators.cpp
@@ -1,3 +1,5 @@
+/*Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), 
+and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.*/
 #include <cmath>
 #include <cstdio>
 #include <vector>
