c04afa7940fd5728868cd87c991cac40222b4146
Roshan Chavan <roshan9722@gmail.com>
2017-09-05 08:24:52 +0530

    Update Day_4_ClassVInstance.cpp

--- Day_4_ClassVInstance.cpp
+++ Day_4_ClassVInstance.cpp
@@ -1,3 +1,13 @@
+/*Write a Person class with an instance variable, , and a constructor that takes an integer,intial age, as a parameter.
+The constructor must assign initial age to age after confirming the argument passed as initial age is not negative;
+if a negative argument is passed as initial age, the constructor should set age to 0 and print Age is not valid,
+setting age to 0.. In addition, you must write the following instance methods:
+
+yearPasses() should increase the age instance variable by 1.
+amIOld() should perform the following conditional actions:
+If age<13, print You are young..
+If  age>=13 and age<18, print You are a teenager..
+Otherwise, print You are old..*/
 using namespace std;
 #include <iostream>
 
